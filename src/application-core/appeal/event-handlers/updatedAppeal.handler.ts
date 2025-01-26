0ea7102a08a08e25a24e7d0e09b811f76f2aa3f1
Carlos Sanjuan <hello@carlossanjuan.co>
2025-01-25 21:56:56 -0500

    feat: handler schedule on update appeals

--- src/application-core/appeal/event-handlers/updatedAppeal.handler.ts
+++ src/application-core/appeal/event-handlers/updatedAppeal.handler.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import {
   AppealDocument,
+  AppealRequest,
+  AppealRequestChange,
   AppealStatus,
 } from '../../../infrastructure/persistence/schema/appeal.schema';
 import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
 import { UserDocument } from '../../../infrastructure/persistence/schema/user.schema';
+import { request } from 'express';
+import { ScheduleDocument } from '../../../infrastructure/persistence/schema/schedule.schema';
+import { ScheduleGateway } from '../../../infrastructure/persistence/gateway/shedule.gateway';
+import { SubjectGateway } from '../../../infrastructure/persistence/gateway/subject.gateway';
+import {
+  SubjectDocument,
+  SubjectGroup,
+} from '../../../infrastructure/persistence/schema/subject.schema';
 
 @Injectable()
 export class UpdatedAppealHandler {
-  constructor(private readonly eventEmitter: EventEmitter2) {}
+  constructor(
+    private readonly scheduleGateway: ScheduleGateway,
+    private readonly subjectGateway: SubjectGateway,
+    private readonly eventEmitter: EventEmitter2,
+  ) {}
 
   @OnEvent('appeal.updated')
   async execute({
@@ -18,10 +32,8 @@ export class UpdatedAppealHandler {
     appeal: AppealDocument;
     user: UserDocument;
   }) {
-    console.log('UpdatedAppealHandler', appeal);
-    console.log('UpdatedAppealHandler', user);
     await this.handleStatus(appeal, user);
-    this.handleSchedule(appeal);
+    await this.handleSchedule(appeal);
   }
 
   private async handleStatus(appeal: AppealDocument, user: UserDocument) {
@@ -36,12 +48,93 @@ export class UpdatedAppealHandler {
     }
   }
 
-  private handleSchedule(appeal: AppealDocument) {
-    // TODO: Cambio al horario del estudiante una vez un to sea aprobado
+  private async handleSchedule(appeal: AppealDocument) {
     if (
       appeal.status === AppealStatus.APPROVED ||
       appeal.status === AppealStatus.PARTIAL_REJECTED
     ) {
+      const schedule: ScheduleDocument = await this.scheduleGateway.findOne({
+        student: appeal.student,
+      });
+
+      if (!schedule) {
+        return;
+      }
+
+      for (const request of appeal.requests) {
+        if (request.from === null && request.to.length) {
+          const to: AppealRequestChange = request.to.find(
+            (to) => to.approved === true,
+          );
+
+          if (!to) {
+            continue;
+          }
+
+          const subject: SubjectDocument = await this.subjectGateway.findOne({
+            sku: to.sku,
+          });
+
+          if (!subject) {
+            continue;
+          }
+
+          const group: SubjectGroup = subject.groups.find(
+            (group) => group.sku === to.group,
+          );
+
+          schedule.subjects.push({
+            name: subject.name,
+            sku: subject.sku,
+            group: {
+              sku: group.sku,
+              schedule: group.schedule,
+            },
+          });
+
+          await schedule.save();
+        } else if (request.to === null && request.from !== null) {
+          schedule.subjects = schedule.subjects.filter(
+            (subject) => subject.sku !== request.from.sku,
+          );
+          await schedule.save();
+        } else if (request.from !== null && request.to.length) {
+          schedule.subjects = schedule.subjects.filter(
+            (subject) => subject.sku !== request.from.sku,
+          );
+
+          const to: AppealRequestChange = request.to.find(
+            (to) => to.approved === true,
+          );
+
+          if (!to) {
+            continue;
+          }
+
+          const subject: SubjectDocument = await this.subjectGateway.findOne({
+            sku: to.sku,
+          });
+
+          if (!subject) {
+            continue;
+          }
+
+          const group: SubjectGroup = subject.groups.find(
+            (group) => group.sku === to.group,
+          );
+
+          schedule.subjects.push({
+            name: subject.name,
+            sku: subject.sku,
+            group: {
+              sku: group.sku,
+              schedule: group.schedule,
+            },
+          });
+
+          await schedule.save();
+        }
+      }
     }
   }
 }
