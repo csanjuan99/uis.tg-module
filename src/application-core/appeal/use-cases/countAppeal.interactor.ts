8acbc92bbad172c86a6947d957773d3c3a29ac58
Esteban Florez <estebandafloto@gmail.com>
2025-07-24 15:48:32 -0500

    adjust  count users and appeals

--- src/application-core/appeal/use-cases/countAppeal.interactor.ts
+++ src/application-core/appeal/use-cases/countAppeal.interactor.ts
@@ -1,13 +1,59 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, Scope, Inject, Logger } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 import { AppealGateway } from '../../../infrastructure/persistence/gateway/appeal.gateway';
+import { UserGateway } from '../../../infrastructure/persistence/gateway/user.gateway';
 import { FilterQuery } from 'mongoose';
 import { Appeal } from '../../../infrastructure/persistence/schema/appeal.schema';
 
-@Injectable()
+interface Program {
+  id: number;
+  name: string;
+  new_pensum: boolean;
+}
+
+interface SessionUser {
+  id: string;
+  name: string;
+  lastname: string;
+  identification?: string;
+  shift?: string;
+  username: string;
+  permissions: string[];
+  kind: string;
+  program: Program;
+}
+
+interface AuthenticatedRequest extends Request {
+  user: SessionUser;
+}
+
+@Injectable({ scope: Scope.REQUEST })
 export class CountAppealInteractor {
-  constructor(private readonly appealGateway: AppealGateway) {}
+  private readonly logger = new Logger(CountAppealInteractor.name);
+
+  constructor(
+    private readonly appealGateway: AppealGateway,
+    private readonly userGateway: UserGateway,
+    @Inject(REQUEST) private readonly request: AuthenticatedRequest,
+  ) {}
 
   async execute(payload?: FilterQuery<Appeal>) {
-    return this.appealGateway.count(payload);
+    const sessionProgramId = this.request.user?.program?.id;
+
+    if (!sessionProgramId) {
+      return 0;
+    }
+
+    const students = await this.userGateway.find({ 'program.id': sessionProgramId }, { _id: 1 });
+    const studentIds = students.map((s) => s._id);
+
+    const enhancedPayload: FilterQuery<Appeal> = {
+      ...payload,
+      student: { $in: studentIds },
+    };
+
+    return this.appealGateway.count(enhancedPayload);
   }
 }
--- src/application-core/user/use-cases/countUsers.interactor.ts
+++ src/application-core/user/use-cases/countUsers.interactor.ts
@@ -1,13 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Scope, Inject, Logger } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 import { UserGateway } from '../../../infrastructure/persistence/gateway/user.gateway';
 import { FilterQuery } from 'mongoose';
 import { User } from '../../../infrastructure/persistence/schema/user.schema';
 
-@Injectable()
+interface Program {
+  id: number;
+  name: string;
+  new_pensum: boolean;
+}
+
+interface SessionUser {
+  id: string;
+  name: string;
+  lastname: string;
+  identification?: string;
+  shift?: string;
+  username: string;
+  permissions: string[];
+  kind: string;
+  program: Program;
+}
+
+interface AuthenticatedRequest extends Request {
+  user: SessionUser;
+}
+@Injectable({ scope: Scope.REQUEST })
 export class CountUsersInteractor {
-  constructor(private readonly userGateway: UserGateway) {}
+  private readonly logger = new Logger(CountUsersInteractor.name);
+
+  constructor(
+    private readonly userGateway: UserGateway,
+    @Inject(REQUEST) private readonly request: AuthenticatedRequest,
+  ) {}
 
   async execute(payload?: FilterQuery<User>) {
-    return this.userGateway.count(payload);
+    const sessionProgramId = this.request.user?.program?.id;
+
+    if (!sessionProgramId) {
+      return 0;
+    }
+
+    const enhancedPayload: FilterQuery<User> = {
+      ...payload,
+      'program.id': sessionProgramId,
+    };
+
+    return this.userGateway.count(enhancedPayload);
   }
 }
